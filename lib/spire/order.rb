4e8df5d3237d1a8411a209fb58ac5b3ad82c885f
Denis <ddujota@internationalsafety.com>
2018-10-25 13:29:09 -0400

    added the register attributes

--- lib/spire/order.rb
+++ lib/spire/order.rb
@@ -1,7 +1,7 @@
 module Spire
 
   class Order < BasicData
-
+    register_attributes :id, :order_no, :customer, :hold, :order_date, :address, :shipping_address, :customer_po, :fob, :terms_code, :terms_text, :freight, :taxes, :subtotal, :subtotal_ordered, :discount, :total_discount, :total, :total_ordered, :gross_profit, :items, :created_by, :modified_by, :created, :modified
   end
 
 end
